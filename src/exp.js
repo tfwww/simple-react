24868760a5a588516c542409b619b7af1590fe4b
Winter Zhong <wmzhong@users.noreply.github.com>
2018-05-23 10:41:43 +0800

    添加 class 类

--- src/exp.js
+++ src/exp.js
@@ -1,11 +1,28 @@
-const button = document.querySelector('.like-btn')
-const buttonText = button.querySelector('.like-text')
-let isLiked = false
-button.addEventListener('click', function () {
-  isLiked = !isLiked
-  if (isLiked) {
-	buttonText.innerHTML = '取消'
-  } else {
-	buttonText.innerHTML = '点赞'
-  }
-}, false)
+// const button = document.querySelector('.like-btn')
+// const buttonText = button.querySelector('.like-text')
+// let isLiked = false
+// button.addEventListener('click', function () {
+// 	isLiked = !isLiked
+// 	if (isLiked) {
+// 		buttonText.innerHTML = '取消'
+// 	} else {
+// 		buttonText.innerHTML = '点赞'
+// 	}
+// }, false)
+
+class LikeButton {
+    render() {
+      return `
+        <button class='like-btn'>
+        	<span class='like-text'>点赞</span>
+        </button>
+      `
+    }
+}
+
+const wrapper = document.querySelector('.wrapper')
+const likeButton1 = new LikeButton()
+wrapper.innerHTML = likeButton1.render()
+
+const likeButton2 = new LikeButton()
+wrapper.innerHTML += likeButton2.render()
